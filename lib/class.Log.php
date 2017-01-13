d0ec3eff3587ec7f6af0df7e767f7d2abec301f2
tom2tom <tpgww@onepost.net>
2017-01-13 21:01:03 +1100

    bump PHP to 5.4+

--- lib/class.Log.php
+++ lib/class.Log.php
@@ -4,7 +4,7 @@
 # Copyright (C) 2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file Auther.module.php
 # More info at http://dev.cmsmadesimple.org/projects/auther
-# Requires PHP 5.3+
+# Requires PHP 5.4+
 #----------------------------------------------------------------------
 namespace Auther;
 
