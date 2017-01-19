df09367fb26d4f87a19a26ca1bdf35b1d6a92616
tom2tom <tpgww@onepost.net>
2017-01-19 17:05:52 +1100

    first working version

--- lib/class.Import.php
+++ lib/class.Import.php
@@ -9,6 +9,8 @@ namespace Auther;
 
 class Import
 {
+	private $cfuncs = FALSE; //Crypter-class object
+
 	private function ToChr($match)
 	{
 		$st = ($match[0][0] == '&') ? 2:1;
@@ -34,7 +36,7 @@ class Import
 			if ($one) {
 				$some = TRUE;
 				$one = trim(preg_replace_callback(
-					array('/&#\d\d;/','/%\d\d%/'),array($this,'ToChr'),$one));
+					['/&#\d\d;/','/%\d\d%/'], [$this,'ToChr'], $one));
 			}
 		}
 		unset($one);
@@ -43,12 +45,39 @@ class Import
 		return FALSE; //ignore lines with all fields empty
 	}
 
+	/*
+	MakeContext:
+	Add a row to the contexts table, using default values
+	No check here for alias-duplication
+	@mod: reference to current Auther module object
+	$context: context identifier number or string
+	@db: reference to database connection
+	@pref: string database tables prefix
+	Returns: int identifier of created context
+	*/
+	private function MakeContext(&$mod, $context, &$db, $pref)
+	{
+		$cid = $db->GenID($pref.'module_auth_contexts_seq');
+		if (is_numeric($context)) {
+			$name = 'Import_data('.$context.')'; //no translation
+		} else {
+			$name = 'Import_'.$context;
+		}
+		$t = strtolower(preg_replace(['/\s+/', '/__+/'], ['_', '_'], $name));
+		$alias = substr($t, 0, 16);
+		$passwd = $this->cfuncs->decrypt_preference($mod, 'default_password');
+
+		$sql = 'INSERT INTO '.$pref.'module_auth_contexts (id,name,alias,default_password) VALUES (?,?,?,?)';
+		$db->Execute($sql, [$cid, $name, $alias, $hash]);
+		return $cid;
+	}
+
 	/**
 	ImportUsers:
 	Import user(s) data from uploaded CSV file. Can handle re-ordered columns.
-	@mod: reference to current Booker module object
-	@id: session identifier
-	Returns: 2-member array, [0] = T/F indicating success, [1] = count of imports, or lang key for message
+	@mod: reference to current Auther module object
+	@id: module ID
+	Returns: 2-member array, [0] = T/F indicating success, [1] = message
 	*/
 	public function ImportUsers(&$mod, $id)
 	{
@@ -61,127 +90,81 @@ class Import
 			 || !($ext == 'csv' || $ext == 'CSV')
 				 || $file_data['size'] <= 0 || $file_data['size'] > 25600 //$max*1000
 				 || $file_data['error'] != 0) {
-				return array(FALSE,'err_file');
+				return [FALSE, $mod->Lang('err_file')];
 			}
 			$fh = fopen($file_data['tmp_name'],'r');
 			if (!$fh)
-				return array(FALSE,'err_perm');
+				return [FALSE, $mod->Lang('err_perm')];
 			//basic validation of file-content
 			$firstline = self::GetSplitLine($fh);
 			if ($firstline == FALSE) {
-				return array(FALSE,'err_file');
+				return [FALSE, $mod->Lang('err_file')];
 			}
 			//file-column-name to fieldname translation
-			$translates = array(
+			$translates = [
+			 '#Context'=>'context', //interpreted
 			 '#Login'=>'publicid',
 			 'Password'=>'passhash', //interpreted
-			 'Passhash'=>'passhash',
-			 'Email'=>'address',
-			 '#Context'=>'context',
-			 'Update'=>'update' //not a real field
-			);
+//			 'Passhash'=>'passhash',
+			 'MessageTo'=>'address',
+			 'Update'=>'update' //not a real field, numeric user-id or some boolean
+			];
 			/* non-public
+				=>addwhen
 				=>lastuse
 				=>active
 			*/
 			$t = count($firstline);
 			if ($t < 1 || $t > count($translates)) {
-				return array(FALSE,'err_file');
+				return [FALSE, $mod->Lang('err_file')];
 			}
 			//setup for interpretation
-			$offers = array(); //column-index to fieldname translator
+			$offers = []; //column-index to fieldname translator
 			foreach ($translates as $pub=>$priv) {
 				$col = array_search($pub,$firstline);
 				if ($col !== FALSE)
 					$offers[$col] = $priv;
 				elseif ($pub[0] == '#') {
 					//name of compulsory fields has '#' prefix
-					return array(FALSE,'err_file');
+					return [FALSE, $mod->Lang('err_file')];
 				}
 			}
-			$utils = new Utils();
-			//for update checks
-			$exist = $utils->SafeGet('SELECT booker_id,name,publicid FROM '.$mod->BookerTable.' ORDER BY booker_id',FALSE);
 
-			$funcs = new Userops();
-			$dt = new \DateTime('now',new \DateTimeZone('UTC'));
-			$st = $dt->getTimestamp();
-//			$skip = FALSE;
+			$pref = \cms_db_prefix();
+			$db = \cmsms()->GetDb();
+			//for update checks
+			$exist = $db->GetArray('SELECT id,publicid FROM '.$pref.'module_auth_users ORDER BY id');
+
+			$utils = new Utils();
+			$afuncs = new Auth($mod); //context [re]set in loop
+			$this->cfuncs = new Crypter();
+
+			$masterkey = $this->cfuncs->decrypt_preference($mod, 'masterpass');;
+			$cached = [];
+			$st = time(); //UTC stamp
+			$skips = 0;
 			$icount = 0;
 
 			while (!feof($fh)) {
 				$imports = self::GetSplitLine($fh);
 				if ($imports) {
-					$data = array();
-					$save = FALSE;
+					$data = [];
 					$update = FALSE;
 					foreach ($imports as $i=>$one) {
 						$k = $offers[$i];
 						if ($one) {
 							switch ($k) {
-							 case 'name':
+							 case 'context':
+							 	if (is_numeric($one)) {
+									$data[$k] = (int)$one;
+								} else {
+	 								$data[$k] = trim($one);
+								}
+								break;
 							 case 'publicid':
-								$data[$k] = trim($one);
-								$save = TRUE;
-								break;
 							 case 'passhash':
- 								$t = trim($one);
-								if ($translates[$i] == 'Password') {
-									$data[$k] = $funcs->HashPassword($t);
-									$save = TRUE;
-								} elseif (empty($data[$k])) { //Passhash but no prior Password
-									$data[$k] = ($t) ? $t : $funcs->HashPassword($t);
-									$save = TRUE;
-								}
-								break;
 							 case 'address':
- 								$t = trim($one);
-								if (!preg_match('/\w+@\w+\.\w+/',$t)) {
-									return array(FALSE,'err_file');
-								}
-								$data[$k] = $t;
-								$save = TRUE;
-								break;
-							 case 'phone':
- 								$t = trim($one);
-						 		if (!preg_match('/^(\+\d{1,4} *)?[\d ]{5,15}$/',$t)) {
-									return array(FALSE,'err_file');
-								}
-								$data[$k] = $t;
-								$save = TRUE;
-								break;
-							 case 'type':
-								switch ($translates[$i]) {
-							 	 case 'Postpayer':
-								 	$t = ($one == 'no' || $one == 'NO') ? 0:10; //permission-flag
-									break;
-								 case 'Recorder':
-								 	$t = ($one == 'no' || $one == 'NO') ? 0:20; //ditto
-									break;
-								 case 'Usertype':
-									if (!is_numeric($one)) {
-										return array(FALSE,'err_file');
-									}
-									$t = (int)$one;
-									if ($t < 0 || $t > 9) //base-types 0..9
-										$t = 0;
-									break;
-								}
-								if (isset($data[$k]))
-									$data[$k] += $t;
-								else
-									$data[$k] = $t;
-								$save = TRUE;
-								break;
-							 case 'displayclass':
-								if (!is_numeric($one)) {
-									return array(FALSE,'err_file');
-								}
-								$t = (int)$one;
-								if ($t < 1 || $t > \Booker::USERSTYLES)
-									$t = 1;
-								$data[$k] = $t;
-								$save = TRUE;
+ 								$data[$k] = trim($one);
 								break;
 							 case 'update':
 							 	if (is_numeric($one)) {
@@ -191,18 +174,10 @@ class Import
 								}
 								break;
 							default:
-								return array(FALSE,'err_file');
+								return [FALSE, $mod->Lang('err_file')];
 							}
 						} else {
 							switch ($k) {
-							 case 'type':
-								if (!isset($data[$k])) {
-									if ($translates[$i] == 'Usertype')
-										$data[$k] = 0;
-								}
-								break;
-							 case 'displayclass':
-								$data[$k] = 1;
 							 case 'update': //ignore this
 								break;
 							 default:
@@ -211,36 +186,62 @@ class Import
 							}
 						}
 					}
+
+					$t = $data['context'];
+					if ($t) {
+						if (array_key_exists($t, $cached)) {
+							$cid = $cached[$t];
+						} else {
+							$cid = $utils->ContextID($t);
+							if ($cid) {
+								$cached[$cid] = $cid;
+							} else {
+								$cid = $this->MakeContext($mod, $t, $db, $pref);
+								$cached[$t] = $cid;
+							}
+						}
+					} elseif (array_key_exists('oopsanon', $cached)) {
+						$cid = $cached['oopsanon'];
+					} else {
+						$cid = $this->MakeContext($mod, $mod->Lang('missingname'), $db, $pref);
+						$cached['oopsanon'] = $cid;
+					}
+
+					$afuncs->setContext($cid);
+
+					$res = $afuncs->validateLogin($data['publicid']);
+					$save = $res[0];
+					$res = $afuncs->validatePassword($data['passhash']);
+					$save = $save && $res[0];
+					$res = $afuncs->validateAddress($data['address']);
+					$save = $save && $res[0];
+
 					if ($save) {
+						$data['passhash'] = $afuncs->password_hash($data['passhash'], $masterkey);
+						$data['address'] = $this->cfuncs->encrypt_value($mod, $data['address'], $masterkey);
+
 						$done = FALSE;
 						if ($update) { //TODO robust UPSERT
 							if (is_numeric($update)) {
-								$sql = 'SELECT booker_id FROM '.$mod->BookerTable.' WHERE booker_id=?';
-								$bookerid = $utils->SafeGet($sql,array($update),'one');
+								$sql = 'SELECT id FROM '.$pref.'module_auth_users WHERE id=?';
+								$uid = $db->GetOne($sql,[$update]);
 							} else {
-								$bookerid = FALSE;
+								$uid = FALSE;
 							}
-							if (!$bookerid) {
-								$sql = 'SELECT booker_id FROM '.$mod->BookerTable;
+							if (!$uid) {
 								if ($data['publicid']) {
-									$sql .= ' WHERE publicid=?';
-									$args = array($data['publicid']);
-									$bookerid = $utils->SafeGet($sql,$args,'one');
-								} elseif ($data['name']) {
-									$sql .= ' WHERE name=?';
-									$args = array($data['name']);
-									$bookerid = $utils->SafeGet($sql,$args,'one');
+									$sql = 'SELECT id FROM '.$pref.'module_auth_users WHERE publicid=?';
+									$uid = $db->GetOne($sql,[$data['publicid']]);
 								} else {
-									$bookerid = FALSE;
+									$uid = FALSE;
 								}
 							}
-							if ($bookerid) {
-								//TODO cache $bookerid=>$data['name'].$data['publicid']
+							if ($uid) {
 								$namers = implode('=?,',array_keys($data));
-								$sql = 'UPDATE '.$mod->BookerTable.' SET '.$namers.'=? WHERE booker_id=?';
+								$sql = 'UPDATE '.$pref.'module_auth_users SET '.$namers.'=? WHERE id=?';
 								$args = array_values($data);
-								$args[] = $bookerid;
-								if ($utils->SafeExec($sql,$args)) {
+								$args[] = $uid;
+								if ($db->Execute($sql,$args)) {
 									$icount++;
 									$done = TRUE;
 								}
@@ -249,29 +250,30 @@ class Import
 						if (!$done) {
 							$namers = implode(',',array_keys($data));
 							$fillers = str_repeat('?,',count($data)-1);
-							$sql = 'INSERT INTO '.$mod->BookerTable.' (booker_id,'.$namers.',addwhen) VALUES (?,'.$fillers.'?,?)';
+							$sql = 'INSERT INTO '.$pref.'module_auth_users (id,'.$namers.',addwhen) VALUES (?,'.$fillers.'?,?)';
 							$args = array_values($data);
-							$bookerid = $mod->dbHandle->GenID($mod->BookerTable.'_seq');
-							array_unshift($args,$bookerid);
+							$uid = $db->GenID($pref.'module_auth_users_seq');
+							array_unshift($args,$uid);
 							$args[] = $st;
-							if ($utils->SafeExec($sql,$args)) {
+							if ($db->Execute($sql,$args)) {
 								$icount++;
 							} else {
-								return array(FALSE,'err_system');
+								return [FALSE, $mod->Lang('err_system')];
 							}
 						}
-//					} else {
-//						$skip = TRUE;
+					} else {
+						$skips++;
 					}
 				}
 			}
 			fclose($fh);
-//			if ($skip)
-//				return array(FALSE,'warn_duplicate');
-//			else
+
+			if ($skips)
+				return [FALSE, $mod->Lang('import_fails', $skips)];
 			if ($icount)
-				return array(TRUE,$icount);
-			return array(FALSE,'none');
+				return [TRUE, $mod->Lang('import_count', $icount)];
+			return [FALSE, $mod->Lang('import_count', 0)];
 		}
-		return array(FALSE,'err_system');
+		return [FALSE, $mod->Lang('err_system')];
 	}
+}
