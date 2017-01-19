436fbc6c6a43860590ee681b9bffdfcfbaa0df24
tom2tom <tpgww@onepost.net>
2017-01-19 17:31:21 +1100

    update import-help

--- lang/en_US.php
+++ lang/en_US.php
@@ -232,11 +232,11 @@ Each line in the file (except the header line, discussed below) represents one u
 <h4>Header line</h4>
 <p>The first line of the file names the fields in the file, as follows.
 The supplied names may be in any order. Those prefixed by a '#' represent compulsory values.<br />
-<code>#Context,#Login,Password,Passhash,MessageTo,Update</code></p>
+<code>#Context,#Login,Password,MessageTo,Update</code></p>
 <h4>Other lines</h4>
 <p>The data in each line must conform to the header columns, of course. Any non-compulsory field, or entire line, may be empty.<br />
-Context may be a numeric identifier, or alias string, representing a login-context (which will be created if not already present).<br />
-If neither a Password or (previously-exported) Passhash value is provided, a default will be applied.<br />
+Context may be a numeric identifier or alias string, representing a login-context (which will be created if not already present).<br />
+If Password is not provided, a default will be applied.<br />
 MessageTo will typically be an email address.<br />
 The Update field will be treated as TRUE if it contains something other than 0 or 'no' or 'NO' (no quotes, untranslated)<br />
 <h3>Problems</h3>
