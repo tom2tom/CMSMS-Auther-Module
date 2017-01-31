d25999280df9627de36dd45b0ffdb4013b5d1fa3
tom2tom <tpgww@onepost.net>
2017-01-31 16:38:21 +1100

    add placeholders for ajax exchanges

--- validate.php
+++ validate.php
@@ -6,6 +6,12 @@
 # More info at http://dev.cmsmadesimple.org/projects/auther
 #----------------------------------------------------------------------
 
+function ajax_errreport($args) {
+	header('HTTP/1.1 500 Internal Server Error');
+	header('Content-Type: application/json; charset=UTF-8');
+	die(json_encode($args));
+}
+
 //clear all page-content echoed before now
 $handlers = ob_list_handlers();
 if ($handlers) {
@@ -27,8 +33,15 @@ foreach ($keys as $kn) {
 $c = max($scan);
 $id = array_search($c, $scan);
 
+$jax = ($_POST[$id.'jsworks'] !== '');
+
 $kn = $id.'data';
 if (empty($_POST[$kn])) {
+	//TODO signal something
+	if ($jax) {
+		ajax_errreport(['TODO']);
+	} else {
+	}
 	exit;
 }
 
@@ -44,39 +57,33 @@ $cfuncs = new Auther\Crypter();
 $pw = $cfuncs->decrypt_preference($mod, 'masterpass');
 $t = openssl_decrypt($_POST[$kn], 'BF-CBC', $pw, 0, $_POST[$id.'IV']);
 if (!$t) {
+	//TODO signal something
+	if ($jax) {
+		ajax_errreport(['TODO']);
+	} else {
+	}
 	exit;
 }
 $params = (array)json_decode($t);
 if (empty($params) || $params['identity'] !== substr($id, 2, 3)) {
+	//TODO signal something
+	if ($jax) {
+		ajax_errreport(['TODO']);
+	} else {
+	}
 	exit;
 }
 
 $db = $gCms->GetDb(); //var defined by inclusion
 $pre = cms_db_prefix();
 $cdata = $db->GetRow('SELECT * FROM '.$pre.'module_auth_contexts WHERE id=?', [$params['context']]);
+//TODO session data from cache
 
 $afuncs = new Auther\Auth($mod, $params['context']);
 
 //TODO get & process other $_POST values
 
-$adbg = json_decode(
-'{"iv":"wvdG7+KjzXKRWuDPgHhfig==",
-"v":1,
-"iter":1000,
-"ks":128,
-"ts":64,
-"mode":"ccm",
-"adata":"This%20is%20my%20nonce",
-"cipher":"aes",
-"salt":"7LAtmkELhSc=",
-"ct":"w1wAR4pw1/v25GXqPWb1BuRIF9B+jImzoNBnqUlKPEnQEZlB4iG57UxaiwuOfq3m1/i7eRGBWBhy"}'
-);
-$adbg2 = $adbg->iv;
-
-$avars = $_POST;
-$X = $Y;
-
-/* sjcl output = string
+/* sjcl longform output = string
 {
 "iv":"wvdG7+KjzXKRWuDPgHhfig==",
 "v":1,
@@ -91,10 +98,22 @@ $X = $Y;
 }
 */
 
-if ($_POST[$id.'jsworks'] !== '') { //TODO && not-finished-now
-//send stuff via ajax to user
+if (1) { //TODO error
+	if ($jax) {
+		ajax_errreport(['message'=>'OOPS, something went wrong!', 'code'=>1337]);
+	} else {
+		//send stuff to $params['handler']
+	}
+} elseif (1) { //TODO not-finished-now
+	if ($jax) {
+		header('HTTP/1.1 204 No Content');
+		header('Content-Type: application/json; charset=UTF-8');
+		die(json_encode(['TODO']));
+	} else {
+		//send stuff to $params['handler']
+	}
 } else {
-//send stuff to $params['handler']
+	//send stuff to $params['handler']
 }
 
 exit;
