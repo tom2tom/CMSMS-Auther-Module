6dba6275628771f1abdad0f743ddcc76fe1705cd
tom2tom <tpgww@onepost.net>
2017-02-16 13:01:43 +1100

    add class contant

--- lib/class.Session.php
+++ lib/class.Session.php
@@ -19,7 +19,8 @@ class Session
 	const BAD_IP = 11;
 	const BLOCKED_IP = 12;
 	const REQUEST_ACTIV = 61;
-	const REQUEST_RESET = 62; //c.f. RESET_REQUESTED?
+	const REQUEST_CHANGE = 62;
+	const REQUEST_RESET = 63; //c.f. RESET_REQUESTED?
 
 	protected $mod;
 	protected $db;
